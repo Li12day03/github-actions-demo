e620095f3f476e68e7596df9f3739ab06eff3b66
李 <li12day@qq.com>
2025-08-14 22:17:49 +0800

    修改

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  publicPath:
+  base:
     process.env.NODE_ENV === "production"
       ? "/github-actions-demo/" // 仓库名
       : "/",
